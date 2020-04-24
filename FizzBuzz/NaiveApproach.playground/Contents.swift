a70fbe64005487f6c071298b79f7f1f8ca0c2af9
Yoan Smit <yoan.smit@gmail.com>
2020-04-24 15:13:29 +0200

    [FizzBuzz] Done

--- FizzBuzz/NaiveApproach.playground/Contents.swift
+++ FizzBuzz/NaiveApproach.playground/Contents.swift
@@ -16,17 +16,55 @@ class FizzBuzzTests: XCTestCase {
         XCTAssertEqual(result, "1")
     }
     
+    func test_transform_returnsFizz_whenPassedValueIsMultipleOfThree() {
+        // When
+        let result = sut.transform(number: 3)
+        
+        // Then
+        XCTAssertEqual(result, "Fizz")
+    }
+    
+    func test_transform_returnsBuzz_whenPassedValueIsMultipleOfFive() {
+        // When
+        let result = sut.transform(number: 5)
+        
+        // Then
+        XCTAssertEqual(result, "Buzz")
+    }
+    
+    func test_transform_returnsFizzBuzz_whenPassedValueIsMultipleOfThreeAndFive() {
+        // When
+        let result = sut.transform(number: 15)
+        
+        // Then
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func test_generate_appliesTransfrom_upToPassedValue() {
+        // When
+        let result = sut.generate(upTo: 5)
+        
+        // Then
+        XCTAssertEqual(result, ["1", "2", "Fizz", "4", "Buzz"])
+    }
 }
 
 class FizzBuzzGenerator {
     func transform(number: Int) -> String {
-        return ""
+        var transformed = ""
+        transformed += (number % 3 == 0) ? "Fizz" : ""
+        transformed += (number % 5 == 0) ? "Buzz" : ""
+        return transformed.isEmpty ? "\(number)" : transformed
     }
     
-    func generate(upTo threshold: Int) -> String {
-        return ""
+    func generate(upTo threshold: Int) -> [String] {
+        return (1...threshold)
+            .map(transform)
     }
 }
+
+// Uncomment to see the beauty of the FizzBuzz up to 20
+//print(FizzBuzzGenerator().generate(upTo: 20).joined(separator: "\n"))
 
 
 // MARK: - Test utils
@@ -43,4 +81,5 @@ class TestObserver: NSObject, XCTestObservation {
 
 let testObserver = TestObserver()
 XCTestObservationCenter.shared.addTestObserver(testObserver)
-XCTestSuite.default.run()
+// Uncomment to run the test suite assocciated with `FizzBuzzTests`
+//FizzBuzzTests.defaultTestSuite.run()
